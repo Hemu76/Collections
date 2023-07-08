7858a900ee68604f60f43e7b0eb20673686f278f
Hemu76 <123863825+Hemu76@users.noreply.github.com>
2023-07-08 15:19:20 +0530

    Update AssignmentAWT.java

--- AssignmentAWT.java
+++ AssignmentAWT.java
@@ -10,22 +10,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.FileInputStream;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 
-public class Awt extends Frame {
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-	public static void main(String args[]) throws Exception {
-		InputStream stream = new FileInputStream("D:\\TEST1\\JavaCollections\\TextFiles\\hey.txt");
-		Reader reader = new InputStreamReader(stream);
+public class Main {
+	static int k=0;
+	public int incr() {
+		return k++;
+		//System.out.println(k);
+	}
+	public int decr() {
+		return k--;
+	}
+	public static void main(String[] args) throws Exception {
+		int k=0;
+		File f_src = new File("C:\\Users\\USER\\eclipse-workspace\\Java Programming\\src\\Files\\EmployeeDetails.txt");
+		FileReader reader = new FileReader(f_src);
+		BufferedReader br = new BufferedReader(reader);
+		ArrayList<Employee> AE = new ArrayList<>();
+		String str;
+		String[] s=new String[5];
+		while((str=br.readLine())!=null) {
+			StringTokenizer st = new StringTokenizer(str,",");
+			int i=0;
+			while(st.hasMoreTokens()) {
+				s[i]=st.nextToken();
+				i++;
+			}
+			AE.add(new Employee(s[0],s[1],s[2],s[3],s[4]));
+		}
 		Frame f = new Frame();
-		f.setBackground(Color.gray);
+		f.setBackground(Color.yellow);
 		f.setVisible(true);
 		f.setLayout(null);
 		f.setSize(700, 700);
@@ -102,6 +120,118 @@ public class Awt extends Frame {
 		f.add(t3);
 		f.add(t4);
 		f.add(t5);
+		t1.setText(AE.get(0).getE_no());
+		t2.setText(AE.get(0).getName());
+		t3.setText(AE.get(0).getJob());
+		t4.setText(AE.get(0).getSal());
+		t5.setText(AE.get(0).getDept());
+		b1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				t1.setText(AE.get(0).getE_no());
+				t2.setText(AE.get(0).getName());
+				t3.setText(AE.get(0).getJob());
+				t4.setText(AE.get(0).getSal());
+				t5.setText(AE.get(0).getDept());
+			}
+		});
+		b2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main m=new Main();
+				try{
+					int x=m.incr();
+					while(x<AE.size() && x>=0) {
+						t1.setText(AE.get(x).getE_no());
+						t2.setText(AE.get(x).getName());
+						t3.setText(AE.get(x).getJob());
+						t4.setText(AE.get(x).getSal());
+						t5.setText(AE.get(x).getDept());
+						break;
+					}
+				}
+				catch(Exception ex) {
+					System.out.println(ex.toString());
+				}
+			}
+		});
+		b3.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Main m=new Main();
+				try {
+					int x=m.decr();
+					while(x<AE.size() && x>=0) {
+					t1.setText(AE.get(x).getE_no());
+					t2.setText(AE.get(x).getName());
+					t3.setText(AE.get(x).getJob());
+					t4.setText(AE.get(x).getSal());
+					t5.setText(AE.get(x).getDept());
+					break;
+					}
+				}
+				catch(Exception ex) {
+					System.out.println(ex.toString());
+				}
+			}
+		});
+		b4.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				t1.setText(AE.get(AE.size()-1).getE_no());
+				t2.setText(AE.get(AE.size()-1).getName());
+				t3.setText(AE.get(AE.size()-1).getJob());
+				t4.setText(AE.get(AE.size()-1).getSal());
+				t5.setText(AE.get(AE.size()-1).getDept());
+			}
+		});
+		b5.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				AE.add(new Employee(t1.getText(),t2.getText(),t3.getText(),t4.getText(),t5.getText()));
+				System.out.println(AE.size());
+				for(Employee ee:AE) {
+					System.out.println(ee.getE_no()+" "+ee.getName()+" "+ee.getJob()+" "+ee.getSal()+" "+ee.getDept());
+				}
+			}
+		});
+	//	System.out.println(AE.size());
+		b6.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String x1=t1.getText();
+				String x2=t2.getText();
+				String x3=t3.getText();
+				String x4=t4.getText();
+				String x5=t5.getText();
+				Employee ee=new Employee(x1,x2,x3,x4,x5);
+				ee.setE_no(x1);
+				ee.setName(x2);
+				ee.setJob(x3);
+				ee.setSal(x4);
+				ee.setDept(x5);
+			}
+		});
+		b7.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String x1=t1.getText();
+				String x2=t2.getText();
+				String x3=t3.getText();
+				String x4=t4.getText();
+				String x5=t5.getText();
+				for(int i=0;i<AE.size();i++) {
+					if(AE.get(i).getE_no()==x1 && AE.get(i).getName()==x2 && AE.get(i).getJob()==x3 && AE.get(i).getSal()==x4 && AE.get(i).getDept()==x5) {
+						AE.remove(AE.get(i));
+					}
+					else {
+						System.out.println("Data Not Found");
+					}
+				}
+			}
+		});
+		b10.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				t1.setText("");
+				t2.setText("");
+				t3.setText("");
+				t4.setText("");
+				t5.setText("");
+			}
+		});
 		b11.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				f.dispose();
@@ -112,5 +242,53 @@ public class Awt extends Frame {
 				f.dispose();
 			}
 		});
+	}
+	public static int count() {
+		int c=0;
+		return c++;
+	}
+}
+class Employee{
+	private String e_no;
+	private String name;
+	private String job;
+	private String sal;
+	private String dept;
+	public Employee(String e_no, String name, String job, String sal, String dept) {
+		this.e_no = e_no;
+		this.name = name;
+		this.job = job;
+		this.sal = sal;
+		this.dept = dept;
+	}
+	public String getE_no() {
+		return e_no;
+	}
+	public void setE_no(String e_no) {
+		this.e_no = e_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getJob() {
+		return job;
+	}
+	public void setJob(String job) {
+		this.job = job;
+	}
+	public String getSal() {
+		return sal;
+	}
+	public void setSal(String sal) {
+		this.sal = sal;
+	}
+	public String getDept() {
+		return dept;
+	}
+	public void setDept(String dept) {
+		this.dept = dept;
 	}
 }
