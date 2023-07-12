4c70a1ba86a94565208e32f5e65469ed2c59c829
Hemu76 <123863825+Hemu76@users.noreply.github.com>
2023-07-12 10:02:54 +0530

    Update NewBankCode.java

--- NewBankCode.java
+++ NewBankCode.java
@@ -1,18 +1,20 @@
 package project;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.Reader;
 import java.time.LocalDate;
+import java.time.Period;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.StringTokenizer;
-import java.time.Period;
+
 public class Main {
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-		Reader f1 = new FileReader("C:\\Users\\USER\\eclipse-workspace\\Java Programming\\src\\Files\\AccountDetails.txt");
-		Reader f2 = new FileReader("C:\\Users\\USER\\eclipse-workspace\\Java Programming\\src\\Files\\TransactionDetails.txt");
+		Reader f1 = new FileReader("D:\\TEST1\\JavaCollections\\TextFiles\\AccountDetails.txt");
+		Reader f2 = new FileReader("D:\\TEST1\\JavaCollections\\TextFiles\\TransactionsDetails.txt");
 		BufferedReader br1 = new BufferedReader(f1);
 		BufferedReader br2 = new BufferedReader(f2);
 		ArrayList<Accounts> aa = new ArrayList<>();
@@ -47,24 +49,27 @@ public class Main {
 					Double.parseDouble(obj2[4])));
 		}
 		br2.close();
-		Collections.sort(at,new Comparator<Transactions>() {
-			public int compare(Transactions t1,Transactions t2) {
+		Collections.sort(at, new Comparator<Transactions>() {
+			public int compare(Transactions t1, Transactions t2) {
 				return t1.getTrans_date().compareTo(t2.getTrans_date());
 			}
 		});
 		ArrayList<Accounts> ac = getAllAccounts(aa);
 		for (Accounts z : ac) {
-			System.out
-					.println(z.getAcc_no() + " " + z.getAcc_type() + " " + z.getAcc_open_date() + " " + z.getAcc_bal());
+			// System.out
+			// .println(z.getAcc_no() + " " + z.getAcc_type() + " " + z.getAcc_open_date() + " " + z.getAcc_bal());
 		}
 		ArrayList<Transactions> atr = getAccountTransactions(at, ac.get(0));
 		for (Transactions tr : atr) {
-			System.out.println(tr.getTrans_id() + " " + tr.getAcc_no() + " " + tr.getTrans_date() + " "
-					+ tr.getTrans_type() + " " + tr.getTrans_amt());
+			// System.out.println(tr.getTrans_id() + " " + tr.getAcc_no() + " " + tr.getTrans_date() + " "
+			// + tr.getTrans_type() + " " + tr.getTrans_amt());
 		}
-		System.out.println("Balance on date: " + getBalanceOnDate(at, aa.get(0), LocalDate.parse("2023-02-19")));
-		System.out.println("Minimum balance of month: " + getMinBalance(at, aa.get(0), LocalDate.parse("2023-04-12")));
-		System.out.println("Rate of Interest: " + getInterest(at, aa.get(0)));
+		// System.out.println("Balance on date: " + getBalanceOnDate(at, aa.get(0), LocalDate.parse("2023-02-19")));
+		// System.out.println("Minimum balance of month: " + getMinBalance(at, aa.get(0),
+		// LocalDate.parse("2023-04-12")));
+		for (int i = 0; i < aa.size(); i++) {
+			System.out.println("Interest: " + getInterest(at, aa.get(i)) + " Account Number: " + aa.get(i).getAcc_no());
+		}
 	}
 
 	public static ArrayList<Accounts> getAllAccounts(ArrayList<Accounts> aa) {
@@ -89,10 +94,10 @@ public class Main {
 		double bal = (double) a.getAcc_bal();
 		int flag = 0;
 		ArrayList<Transactions> att = getAccountTransactions(at, a);
-		for (int i = 0; i < att.size(); i++) {
+		for (int i = att.size() - 1; i >= 0; i--) {
 			if (a.getAcc_open_date().compareTo(ld) <= 0) {
 				flag = 1;
-				if (att.get(i).getTrans_date().compareTo(ld) <= 0) {
+				if (att.get(i).getTrans_date().compareTo(ld) >= 0) {
 					if (att.get(i).getTrans_type().equals("deposit")) {
 						bal -= att.get(i).getTrans_amt();
 					} else {
@@ -108,20 +113,13 @@ public class Main {
 	}
 
 	public static double getMinBalance(ArrayList<Transactions> tr, Accounts a, LocalDate m) {
-		if(m.getMonth().compareTo(a.getAcc_open_date().getMonth())==0){
-			return getBalanceOnDate(tr, a, m);
-		}
-		LocalDate mm = LocalDate.of(m.getYear(), m.getMonthValue() , 10);
+		LocalDate mm = LocalDate.of(m.getYear(), m.getMonthValue() + 1, 10);
 		double bal = getBalanceOnDate(tr, a, mm);
 		double min = getBalanceOnDate(tr, a, mm);
-		//System.out.println(bal+"Helloo");
-
-		double minn = 0;
-		int flag=0;
 		ArrayList<Transactions> att = getAccountTransactions(tr, a);
-		for (int i = 0; i < att.size(); i++) {
-			if (att.get(i).getTrans_date().getMonthValue() == m.getMonthValue() 
-					&& att.get(i).getTrans_date().getYear() == m.getYear() && m.getDayOfMonth()>10) {
+		for (int i = att.size() - 1; i >= 0; i--) {
+			if (att.get(i).getTrans_date().getMonthValue() == m.getMonthValue()
+					&& att.get(i).getTrans_date().getYear() == m.getYear() && m.getDayOfMonth() > 10) {
 				if (att.get(i).getTrans_type().equals("withdraw")) {
 					min += att.get(i).getTrans_amt();
 				} else {
@@ -129,50 +127,41 @@ public class Main {
 				}
 			}
 			if (min < bal) {
-				flag=1;
-				minn = min;
+				bal = min;
 			}
 		}
-		//System.out.println(bal+" c"+minn+"vv"+min);
-		if(flag==1) {
-			return Math.min(bal, minn);
-		}
-		return bal;
+		return Math.min(bal, min);
 	}
 
 	public static double getInterest(ArrayList<Transactions> tr, Accounts a) {
 		LocalDate l = LocalDate.now();
-		LocalDate l1=l;
-		double rate = 4.5 / 6;
-		double interest=0;
-		double prince_amt=0;
-		int n=1;
-		Period period=Period.between(a.getAcc_open_date(),l);
-		int diff=(int) period.toTotalMonths();
-		while(n<=6) {
-			if(a.getAcc_open_date().getDayOfMonth()<=10 && a.getAcc_open_date().compareTo(l1) <= 0 ) {
+		LocalDate l1 = l;
+		double rate = 4.5 / 12;
+		double interest = 0;
+		double prince_amt = 0;
+		int n = 1;
+		Period period = Period.between(a.getAcc_open_date(), l);
+		int diff = (int) period.toTotalMonths();
+		while (n <= 6) {
+			if (a.getAcc_open_date().getDayOfMonth() <= 10 && a.getAcc_open_date().compareTo(l1) <= 0) {
 				prince_amt = getMinBalance(tr, a, l);
-				System.out.println(prince_amt+" "+l.getMonthValue());
+				System.out.println(prince_amt + " " + l.getMonthValue());
 				interest += ((prince_amt * rate) / 1200);
-				l=l.minusMonths(1);
-				//System.out.println(interest+"hello");
-				if((diff+1)==n) {
+				l = l.minusMonths(1);
+				if ((diff + 1) == n) {
 					break;
 				}
-			}
-			else if(a.getAcc_open_date().getDayOfMonth()>10 && a.getAcc_open_date().compareTo(l1) <= 0 ){
+			} else if (a.getAcc_open_date().getDayOfMonth() > 10 && a.getAcc_open_date().compareTo(l1) <= 0) {
 				prince_amt = getMinBalance(tr, a, l);
 				interest += (prince_amt * rate) / 1200;
-				l=l.minusMonths(1);
-				//System.out.println(interest+"hello");
-				if(diff==n) {
+				l = l.minusMonths(1);
+				if (diff == n) {
 					break;
 				}
-			}
-			else {
+			} else {
 				prince_amt = getMinBalance(tr, a, l);
 				interest += (prince_amt * rate) / 1200;
-				l=l.minusMonths(1);
+				l = l.minusMonths(1);
 			}
 			n++;
 		}
