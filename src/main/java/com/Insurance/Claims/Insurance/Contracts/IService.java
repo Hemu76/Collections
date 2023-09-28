3e6db16e2b6acc09253a7300b347938282953950
Hemu76 <123863825+Hemu76@users.noreply.github.com>
2023-09-28 18:18:39 +0530

    Add files via upload

---
+++ src/main/java/com/Insurance/Claims/Insurance/Contracts/IService.java
@@ -0,0 +1,65 @@
+package com.Insurance.Claims.Insurance.Contracts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.Insurance.Claims.Insurance.Models.Claim;
+import com.Insurance.Claims.Insurance.Models.ClaimApplication;
+import com.Insurance.Claims.Insurance.Models.ClaimBills;
+import com.Insurance.Claims.Insurance.Models.ClaimHistory;
+import com.Insurance.Claims.Insurance.Models.CoveredDiseases;
+import com.Insurance.Claims.Insurance.Models.PolicyMembers;
+import com.Insurance.Claims.Insurance.Models.ReUpload;
+import com.Insurance.Claims.Insurance.Models.Uploads;
+
+public interface IService {
+
+	ArrayList<Claim> getAllClaims();
+
+	ArrayList<Claim> getFilteredClaims(String status);
+
+	Claim getClaimById(int clamId);
+
+	ArrayList<Claim> viewAllClaims();
+
+	Claim viewClaimById(int clamId);
+
+	int editClaimById(int clamId, String clamRemarks, String clamStatus);
+
+	void addClaim(int i, double requestAmount);
+
+	Claim getClaimByid(int clamIplcId);
+
+	void addClaimBills(String f, String filePath, int cid);
+
+	ArrayList<ClaimBills> viewClaimDocsById(int clamId);
+
+	ClaimBills viewdocBills(int billIndex);
+
+	void addClaimApplication(ClaimApplication application);
+
+	List<ClaimApplication> getAllApplicantions();
+
+	Claim getCliamByPolicy(int id);
+
+	void updateClaimBill(ClaimBills bill);
+
+	List<CoveredDiseases> getAllDiseasesCovered(int id);
+
+	void editClaimById(int clamId, String clamRemarks, String clamStatus, String clamProcessedAmount);
+
+	void updateClaimDate(int clamId);
+
+	List<PolicyMembers> getPlcyMembers();
+
+	List<ClaimHistory> getHistory(int cid);
+
+	List<String> getFamilyByPolicy(int id);
+
+	void addRequiredUpload(ReUpload requiredUpload);
+
+	List<ReUpload> getAllReUploads();
+
+	void addUploads(Uploads up);
+
+}
---
+++ src/main/java/com/Insurance/Claims/Insurance/Repository/ClaimService.java
@@ -0,0 +1,182 @@
+package com.Insurance.Claims.Insurance.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Insurance.Claims.Insurance.Contracts.ClaimsDao;
+import com.Insurance.Claims.Insurance.Contracts.IService;
+import com.Insurance.Claims.Insurance.Models.Claim;
+import com.Insurance.Claims.Insurance.Models.ClaimApplication;
+import com.Insurance.Claims.Insurance.Models.ClaimBills;
+import com.Insurance.Claims.Insurance.Models.ClaimHistory;
+import com.Insurance.Claims.Insurance.Models.CoveredDiseases;
+import com.Insurance.Claims.Insurance.Models.PolicyMembers;
+import com.Insurance.Claims.Insurance.Models.ReUpload;
+import com.Insurance.Claims.Insurance.Models.Uploads;
+
+@Service
+public class ClaimService implements IService {
+	@Autowired
+	ClaimsDao cdao;
+
+	@Override
+	public ArrayList<Claim> getAllClaims() {
+		// TODO Auto-generated method stub
+		System.out.println("hem");
+		return (ArrayList<Claim>) cdao.getAllClaims();
+	}
+
+	@Override
+	public ArrayList<Claim> getFilteredClaims(String status) {
+		// TODO Auto-generated method stub
+		return (ArrayList<Claim>) cdao.getFilteredClaims(status);
+	}
+
+	@Override
+	public Claim getClaimById(int clamId) {
+		// TODO Auto-generated method stub
+		return cdao.getClaimById(clamId);
+	}
+
+	@Override
+	public void addClaim(int i, double requestAmount) {
+		cdao.setClaim(i, requestAmount);
+
+	}
+
+	@Override
+	public Claim getClaimByid(int clamIplcId) {
+		// TODO Auto-generated method stub
+		return cdao.getClaimByid(clamIplcId);
+	}
+
+	@Override
+	public void addClaimBills(String f, String filePath, int cid) {
+		// TODO Auto-generated method stub
+		cdao.setDocs(f, filePath, cid);
+	}
+
+	@Override
+	public ArrayList<ClaimBills> viewClaimDocsById(int clamId) {
+		// TODO Auto-generated method stub
+		return cdao.getDocs(clamId);
+	}
+
+	// Insurance------------
+
+	@Override
+	public ArrayList<Claim> viewAllClaims() {
+		// TODO Auto-generated method stub
+		return (ArrayList<Claim>) cdao.viewAllClaims();
+	}
+
+	@Override
+	public Claim viewClaimById(int clamId) {
+		// TODO Auto-generated method stub
+		return cdao.viewClaimById(clamId);
+	}
+
+	@Override
+	public int editClaimById(int clamId, String clamRemarks, String clamStatus) {
+		// TODO Auto-generated method stub
+		return cdao.editClaimById(clamId, clamRemarks, clamStatus);
+	}
+
+	@Override
+	public ClaimBills viewdocBills(int billIndex) {
+		// TODO Auto-generated method stub
+		return cdao.getDocBills(billIndex);
+	}
+
+	@Override
+	public void addClaimApplication(ClaimApplication application) {
+		cdao.addClaimApplication(application);
+
+	}
+
+	@Override
+	public List<ClaimApplication> getAllApplicantions() {
+
+		return cdao.getAllApplications();
+	}
+
+	@Override
+	public Claim getCliamByPolicy(int id) {
+
+		return cdao.getClaimByPolicy(id);
+	}
+
+	@Override
+	public void updateClaimBill(ClaimBills bill) {
+
+		cdao.updateClaimBill(bill);
+
+	}
+
+	@Override
+	public List<CoveredDiseases> getAllDiseasesCovered(int id) {
+
+		return cdao.getAllCoveredDiseases(id);
+	}
+
+	@Override
+	public void editClaimById(int clamId, String clamRemarks, String clamStatus, String clamProcessedAmount) {
+
+		cdao.updateClaimBill(clamId, clamRemarks, clamStatus, clamProcessedAmount);
+	}
+
+	@Override
+	public void updateClaimDate(int clamId) {
+		cdao.updateDate(clamId);
+
+	}
+
+	@Override
+	public List<PolicyMembers> getPlcyMembers() {
+		// TODO Auto-generated method stub
+		return cdao.getPoliMem();
+	}
+
+	@Override
+	public List<ClaimHistory> getHistory(int cid) {
+		// TODO Auto-generated method stub
+		return cdao.getHistory(cid);
+	}
+
+	@Override
+	public List<String> getFamilyByPolicy(int id) {
+		List<PolicyMembers> members = cdao.getPoliMem();
+		List<String> names = new ArrayList<>();
+		for (PolicyMembers mem : members) {
+
+			if (mem.getInsurancePolicyId() == id) {
+				names.add(mem.getMemberName());
+			}
+		}
+		return names;
+	}
+
+	@Override
+	public void addRequiredUpload(ReUpload requiredUpload) {
+		cdao.addRequiredUploads(requiredUpload);
+
+	}
+
+	@Override
+	public List<ReUpload> getAllReUploads() {
+		List<ReUpload> list = cdao.getAllReUploads();
+
+		return list;
+	}
+
+	@Override
+	public void addUploads(Uploads up) {
+		System.out.println("cdao");
+		cdao.addUploads(up);
+
+	}
+
+}
